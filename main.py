8f1204ff308c8e37059c509968919728f46c0db6
LenCewa <len13@hotmail.de>
2022-01-07 20:42:06 +0100

    Added main function as our entrypoint

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
+def main():
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+    var = 'World!'
+    print(f'Hello {var}')
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
