1fa1258841ed21a74b3912b0227650935259e325
LenWilliamson <lencewa13@gmail.com>
2022-01-15 21:47:50 +0100

    Added Link to Data Science Course

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def main():
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# See Pandas Intro for quick infos https://github.com/efldatascience/ds-courses
