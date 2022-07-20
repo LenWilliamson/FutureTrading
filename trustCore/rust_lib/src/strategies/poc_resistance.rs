0328b3410e4c9b66429b18d37ecc17efd762f8d7
LenWilliamson <lencewa13@gmail.com>
2022-07-20 22:44:59 +0200

    POC Resistance strategy improved

--- trustCore/rust_lib/src/strategies/poc_resistance.rs
+++ trustCore/rust_lib/src/strategies/poc_resistance.rs
@@ -8,13 +8,14 @@
  *  - Strategie:
  *      - Annahme: Kurs bewegt sich aufwärts in Richtung POC und prallt nach unten ab
  *      - ENTRY: POC
- *      - Stop-Loss: POC + X€
+ *      - Stop-Loss 1 (SL 1): POC + X€
+ *      - Stop-Loss 2 (SL 2): HIGH der Vorwoche (außer HIGH == POC)
  *      - Take Profit 1 (TP1): "Schlusskurs der Vorwoche (Freitag) + X€"
- *      - Take Profit 2 (TP2): "Schlusskurs der Vorwoche (Freitag) - X€"
- *      - Take Profit 3 (TP3): HIGH der Vorwoche (genauer zu definieren, falls es über dem POC liegt)
+ *      - Take Profit 2 (TP2): "Schlusskurs der Vorwoche (Freitag)"
+ *      - Take Profit 3 (TP3): "Schlusskurs der Vorwoche (Freitag) - X€"
  *      - TODO: Wie legen wir X fest? Soll X überall gleich sein?
- *      - EXIT: Schlusskurs der aktuellen Woche am Freitag 23:59 Uhr, wenn in der Woche weder
- *          - Stop-Loss
+ *      - TIMEOUT: Schlusskurs der aktuellen Woche am Freitag 23:00 Uhr, wenn in der Woche weder
+ *          - SL1 - SL2
  *          - TP1 - TP3
  *        getriggerd wurde
  * Long:
@@ -23,19 +24,21 @@
  *  - Strategie:
  *      - Annahme: Der Kurs bewegt sich abwärts in Richtung POC und prallt nach oben ab
  *      - ENTRY: POC
- *      - Stop-Loss: POC - X€
+ *      - Stop-Loss 1 (SL 1): POC - X€
+ *      - Stop-Loss 2 (SL 2): LOW der Vorwoche (außer LOW == POC)
  *      - Take Profit 1 (TP1): "Schlusskurs der Vorwoche (Freitag) - X€"
- *      - Take Profit 2 (TP2): "Schlusskurs der Vorwoche (Freitag) + X€"
- *      - Take Profit 3 (TP3): LOW der Vorwoche (genauer zu definieren, falls es unter dem POC liegt)
+ *      - Take Profit 2 (TP2): "Schlusskurs der Vorwoche (Freitag)"
+ *      - Take Profit 3 (TP3): "Schlusskurs der Vorwoche (Freitag) + X€"
  *      - TODO: Wie legen wir X fest? Soll X überall gleich sein?
- *      - EXIT: Schlusskurs der aktuellen Woche am Freitag 23:59 Uhr, wenn in der Woche weder
- *          - Stop-Loss
+ *      - TIMEOUT: Schlusskurs der aktuellen Woche am Freitag 23:00 Uhr, wenn in der Woche weder
+ *          - SL1 - SL2
  *          - TP1 - TP3
  *        getriggerd wurde
  * 
  * Verfeinerungen:
  *  - ATR für TP-Strategien
  *  - ENTRY muss bis Zeitpunkt T (bspw. Mittwochs 18:00) erreicht werden. Sonst findet kein Trade statt.
+ *  - X€ durch Anzahl Ticks ersetzen
  */
 
 pub fn compute() {
